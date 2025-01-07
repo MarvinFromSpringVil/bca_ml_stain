362a34a387f61dc9d028c94a30ad07afadea5e24
HanSolo324 <han324solo@gmail.com>
2025-01-06 19:35:59 -0500

    feat: update model

--- models.py
+++ models.py
@@ -19,9 +19,9 @@ class Unet(nn.Module):
         self.down3 = ConvPool(fch_base * 2, fch_base * 4, isBN)
         self.down4 = ConvPool(fch_base * 4, fch_base * 8, isBN)
         
-        self.encoder = ConvPool(fch_base * 8, fch_base * 16, isBN)
+        self.encoder = ConvPool(fch_base * 8+1, fch_base * 16, isBN)
         
-        self.up1 = UpsampleConv(fch_base * 16 , fch_base * 8, isDeconv, isBN)
+        self.up1 = UpsampleConv(fch_base * 16, fch_base * 8, isDeconv, isBN)
         self.up2 = UpsampleConv(fch_base * 8, fch_base * 4, isDeconv, isBN)
         self.up3 = UpsampleConv(fch_base * 4, fch_base * 2 , isDeconv, isBN)
         self.up4 = UpsampleConv(fch_base * 2, fch_base, isDeconv, isBN)
@@ -32,12 +32,17 @@ class Unet(nn.Module):
                                      self.down4, self.encoder, self.up1, self.up2,\
                                      self.up3, self.up4, self.out])
 
-    def forward(self, input_):
+    def forward(self, input_, condition):
         d1 = self.down1(input_)
         d2 = self.down2(d1)
         d3 = self.down3(d2)
         d4 = self.down4(d3)
-        enc = self.encoder(d4)
+
+        #print(d4.shape) # 1 128 28 28 
+        d5 = torch.cat([d4, condition], dim=1)
+        #enc = self.encoder(d4)
+        enc = self.encoder(d5)
+        
         u1 = self.up1(enc, d4)
         u2 = self.up2(u1, d3)
         u3 = self.up3(u2, d2)
@@ -53,4 +58,3 @@ def get_model(ckpt=None):
         model.load_state_dict(torch.load(ckpt))
 
     return model 
-
